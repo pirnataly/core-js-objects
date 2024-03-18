3f82fe870f9f2c62f117b610504901823df3ba5e
pirnataly <piroznatal@gmail.com>
2024-03-18 21:43:50 +0300

    feat: solve remove prop

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -55,8 +55,12 @@ function mergeObjects(objects) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, 'age') => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  const object = { ...obj };
+  keys.forEach((key) => {
+    delete object[key];
+  });
+  return object;
 }
 
 /**
